12c80f8fdb5da870b528edbc0844ec0c9f1a2693
jaspercurry <jaspercurry@gmail.com>
2016-08-13 15:43:03 -0400

    made sinatra blog platform

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  get '/posts' do
+    @post = Post
+    erb :index
+  end
+
+  post '/posts' do
+    Post.create(params)
+    @post = Post
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts/:id/edit' do
+
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+
+    post = Post.find(params[:id])
+    post.content=(params[:content])
+    post.name=(params[:name])
+    post.save
+    @post = post
+    erb :show
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+
+    post = Post.find(params[:id])
+    @name = post.name
+    post.destroy
+    erb :delete
+  end
+
+
+
+
+
 end
